b5a88c7f6ae1a09d8c9a5f73563d65630f5ab737
Jay-N-Chachapara <jay.chachapara.ce@gmail.com>
2022-01-21 17:17:48 +0530

    Commited on 21st Jan, 2022

---
+++ PascalTriangle.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+     int row=0,i=0,j=0;
+     printf("Enter numer of rows : ");
+     scanf("%d",&row);
+     for(i=0;i<=limit;i++)
+     {
+          for(j=0;j!=i;j++)
+               printf(" ");
+          for(j=0;j+i<=limit;j++)
+          {
+               if(j=0 || j==limit) 
+                    print("1")
+               
+          }
+     }
+     return 0;
+}
